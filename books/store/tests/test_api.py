7a7a76267fe5eb1ded75128f7b03299b6f59dc14
DKowalski25 <dshinoda25@gmail.com>
2024-02-21 19:06:54 +0700

    Add first tests

---
+++ books/store/tests/test_api.py
@@ -0,0 +1,17 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from store.models import Book
+from store.serializers import BooksSerializer
+
+
+class BooksApiTestCase(APITestCase):
+    def test_get(self):
+        book1 = Book.objects.create(name='Test book 1', price=25)
+        book2 = Book.objects.create(name='Test book 2', price=55)
+        url = reverse('book-list')
+        response = self.client.get(url)
+        serializer_date = BooksSerializer([book1, book2], many=True).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_date, response.data)
